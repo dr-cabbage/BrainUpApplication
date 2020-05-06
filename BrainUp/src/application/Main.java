072aaa6964e7a35311b7a5624d6e7afb5fb92c5d
kingk <kingk@DESKTOP-J0RC7GP>
2020-05-06 15:46:46 -0500

    I'll delete this later... 5/6/2020 3:46

--- BrainUp/src/application/Main.java
+++ BrainUp/src/application/Main.java
@@ -1,15 +1,21 @@
 package application;
 	
+import java.nio.file.Paths;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.Parent;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
+		music();
 		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 		
 		Scene scene = new Scene(root, 600, 600);
@@ -19,6 +25,14 @@ public class Main extends Application {
 		
 	}
 	
+	MediaPlayer mediaPlayer;
+	public void music() {
+		String s = "data/Wii.mp3";
+		Media h = new Media(Paths.get(s).toUri().toString());
+		mediaPlayer = new MediaPlayer(h);
+		mediaPlayer.seek(Duration.ZERO);
+		mediaPlayer.play();
+	}
 
 	public static void main(String[] args) {
 		
