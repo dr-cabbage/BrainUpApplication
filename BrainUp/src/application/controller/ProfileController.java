93ac08ad7aabaf22d534197db2e38eed48a68267
Jose Morales <josefmt18@gmail.com>
2020-05-07 01:59:19 -0500

    Update ProfileController.java
    Finished ProfileController.java

--- BrainUp/src/application/controller/ProfileController.java
+++ BrainUp/src/application/controller/ProfileController.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
@@ -65,21 +64,74 @@ public class ProfileController implements Initializable{
 	
 	@FXML protected void handleEditFirst(ActionEvent e) throws IOException {
 		TextInputDialog dialog = new TextInputDialog();
-		dialog.setTitle("Change Name");
-		//dialog.setHeaderText("Change the class name, current name is : "  + name.getText());
-		dialog.setContentText("New Name: ");
+		dialog.setTitle("Change First Name");
+		dialog.setHeaderText("Change your first name, current first name is : "  + firstName.getText());
+		dialog.setContentText("New First Name: ");
 		Optional<String> result = dialog.showAndWait();
 		Login log = new Login();
-		//log.changeClassName(Integer.valueOf(classcode.getText()), result.get());
+		String name = result.get() + " " + lastName.getText();
+		log.editInfo(userName.getText(), name, userName.getText(), password.getText());
 		Stage stage;
 		Parent r;
 		stage = (Stage) root.getScene().getWindow();
-		r = FXMLLoader.load(getClass().getResource("editClass.fxml"));
+		r = FXMLLoader.load(getClass().getResource("profile.fxml"));
 		Scene scene = new Scene(r);
 		stage.setScene(scene);
 		stage.show();
 	}
 	
+	@FXML protected void handleEditLast(ActionEvent e) throws IOException {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("Change Last Name");
+		dialog.setHeaderText("Change your last name, current last name is : "  + lastName.getText());
+		dialog.setContentText("New Last Name: ");
+		Optional<String> result = dialog.showAndWait();
+		Login log = new Login();
+		String name = firstName.getText() + " " + result.get();
+		log.editInfo(userName.getText(), name, userName.getText(), password.getText());
+		Stage stage;
+		Parent r;
+		stage = (Stage) root.getScene().getWindow();
+		r = FXMLLoader.load(getClass().getResource("profile.fxml"));
+		Scene scene = new Scene(r);
+		stage.setScene(scene);
+		stage.show();
+	}
 	
+	@FXML protected void handleEditUser(ActionEvent e) throws IOException {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("Change Username");
+		dialog.setHeaderText("Change your username, current username is : " + userName.getText());
+		dialog.setContentText("New Username: ");
+		Optional<String> result = dialog.showAndWait();
+		Login log = new Login();
+		String name = firstName.getText() + " " + lastName.getText();
+		log.editInfo(userName.getText(), name, result.get(), password.getText());
+		Stage stage;
+		Parent r;
+		stage = (Stage) root.getScene().getWindow();
+		r = FXMLLoader.load(getClass().getResource("profile.fxml"));
+		Scene scene = new Scene(r);
+		stage.setScene(scene);
+		stage.show();
+	}
 	
+	@FXML protected void handleEditPass(ActionEvent e) throws IOException {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("Change Password");
+		dialog.setHeaderText("Change your password, current password is : " + password.getText());
+		dialog.setContentText("New Password: ");
+		Optional<String> result = dialog.showAndWait();
+		Login log = new Login();
+		String name = firstName.getText() + " " + lastName.getText();
+		log.editInfo(userName.getText(), name, userName.getText(), result.get());
+		Stage stage;
+		Parent r;
+		stage = (Stage) root.getScene().getWindow();
+		r = FXMLLoader.load(getClass().getResource("profile.fxml"));
+		Scene scene = new Scene(r);
+		stage.setScene(scene);
+		stage.show();
+	}
+
 }
